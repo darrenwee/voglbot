d3d89f06a5832d0f4e0d1854687b552a48c7b2e5
Darren Wee <darrenweezy@gmail.com>
2016-05-22 17:37:30 +0800

    added new file for /help queries and store documentation

---
+++ helper.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+"""
+	stores command documentation for /help queries
+"""
+
+commandList = {
+	'/add': 'usage: /add <house> <name>\nadd a new freshman <name> from <house>',
+	'/remove': 'usage: /remove <house> <name>\nremoves a freshman <name> from <house>',
+}
+
+def getHelp(command):
+	if command in commandList:
+		return commandList.get(command)
+	else:
+		return 'Command not found.'

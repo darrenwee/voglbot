b1fc3d5ec541740a96e9c6dedceabdf5b6156b94
Darren Wee <darrenweezy@gmail.com>
2016-05-23 19:22:23 +0800

    added proper docs for /add, /remove, /find, /enumerate and naiveHelp function

--- helper.py
+++ helper.py
@@ -2,16 +2,65 @@
 
 """
 	stores command documentation for /help queries
+	
+	##### do not terminate the description, examples and parameters dictionaries with newlines ######
 """
 
-commandList = {
-	'add': 'usage: /add <house> <name>\nadd a new freshman <name> from <house>',
-	'remove': 'usage: /remove <house> <name>\nremoves a freshman <name> from <house>',
-	'find': 'usage: /find <house> <name>\nfinds a freshman <name> from <house>',
+# command description list (do not terminate with newlines)
+description = {
+	'add': 'usage: /add <house> <name>\n--> add a new freshman <name> from <house>',
+	'remove': 'usage: /remove <house> <name>\n--> removes a freshman <name> from <house>',
+	'find': 'usage: /find <house> <name>\n--> finds a freshman <name> from <house> and replies with his/her details. <name> need not be exact.',
+	'enumerate': 'usage: /enumerate <house> <mode>\n--> enumerates all the members of <house> with status <mode>',
+}
+
+# example usage (do not terminate with newlines)
+examples = {
+	'add': 'e.g. /add black john doe augustus lim xiao hua\n--> adds \'john doe augustus lim xiao hua\' to black house',
+	'remove': 'e.g. /remove black john doe augustus lim xiao hua\n--> removes \'john doe augustus lim xiao hua\' to black house',
+	'find': 'e.g. /find black john doe augustus lim xiao hua\n--> finds \'john doe augustus lim xiao hua\' in black house',
+	'enumerate': 'e.g. /enumerate green present\n--> gives a list of all students from green house who are present',
+}
+
+# argument list
+parameterDict = {
+	'group': '<group> -- \'freshmen\', \'ogls\', \'fopcomm\'',
+	'house': '<house> -- \'green\', \'black\', \'purple\', \'blue\', \'red\', \'orange\', \'all\'',
+	'mode': '<mode> -- \'present\', \'absent\', \'total\''
+}
+
+# relevant parameter list
+relevantParameters = {
+	'add': ['house'],
+	'remove': ['house'],
+	'find': ['house'],
+	'enumerate': ['house', 'mode'],
 }
 
 def getHelp(command):
-	if command in commandList:
-		return commandList.get(command)
+	reply = ''
+
+	# check if basic doc exists
+	if description.has_key(command):
+		reply += description.get(command) + '\n\n'
+
+		# append the example if there is one
+		if examples.has_key(command):
+			reply += examples.get(command) + '\n\n'
+
+		# append parameters if there are any
+		if relevantParameters.has_key(command):
+			reply += 'Parameters\n'
+			for param in relevantParameters.get(command):
+				reply += '%s\n' % (parameterDict.get(param))
 	else:
 		return 'Command not found.'
+
+	return reply
+
+def naiveHelp():
+	reply = 'Commands available:'
+	for command in description.keys():
+		reply += command + '\n'
+	reply += 'Use \'/help <command>\' for more info'
+	return reply
